70f325b852c9ee4d57f03e92d8d4ef2bbf315c31
inputTitleHere <seanbryan97@naver.com>
2023-05-07 20:43:13 +0900

    feature/mainPage 메인 페이지 UI개발중 - 헤더 영역의 버튼 생성.

--- creators-notbook-frontend/src/pages/common/header/Header.jsx
+++ creators-notbook-frontend/src/pages/common/header/Header.jsx
@@ -1,21 +1,22 @@
-import './Header.scss'
-import { useSelector } from 'react-redux';
+import "./Header.scss";
+import { useSelector } from "react-redux";
+import LoginRegisterComponent from "./components/LoginRegisterComponent";
+import UserInfoComponent from "./components/UserInfoComponent";
 
 /**
- * 헤더용 컴포넌트. 
+ * 헤더용 컴포넌트.
  * 로그인 여부에 따라 경로 표시를 다르게 한다.
  * @returns <Header/>
  */
-export default function Header(){
-  const user = useSelector((state)=>state.user.value);
+export default function Header() {
+  const user = useSelector((state) => state.user.value);
 
-
-  return(
+  return (
     <div id="header">
       <h1>창작자의 노트북</h1>
       <div className="user-section">
-
+        {user ? <UserInfoComponent /> : <LoginRegisterComponent />}
       </div>
     </div>
-  )
+  );
 }
--- creators-notbook-frontend/src/pages/common/header/components/LoginRegisterComponent.jsx
+++ creators-notbook-frontend/src/pages/common/header/components/LoginRegisterComponent.jsx
@@ -1,9 +1,18 @@
 import { Link } from "react-router-dom";
 
+/**
+ * 로그인, 회원가입 페이지로 이동하는 링크를 배치.
+ * @returns
+ */
 export default function LoginRegisterComponent() {
   return (
-    <>
-      <Link to={"/login"}>로그인</Link> | <Link to={"/register"}>회원가입</Link>
-    </>
+    <div className="not-logged-in">
+      <div className="login">
+        <Link to={"/login"}>로그인</Link>
+      </div>
+      <div className="register">
+        <Link to={"/register"}>회원가입</Link>
+      </div>
+    </div>
   );
 }
